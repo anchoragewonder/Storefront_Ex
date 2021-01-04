2cc3752921ffe7859585b860f01dbd3ff4354fbb
anchoragewonder <anchoragewonder@gmail.com>
2021-01-04 15:15:50 -0500

    adjusting gamelist and json creation

--- storefront-app/src/VideoGameGrid.js
+++ storefront-app/src/VideoGameGrid.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { LikeButton } from './like_button';
+import { GameImg } from './GameImg';
 
 export class VideoGameGrid extends React.Component {
     constructor(props) {
@@ -11,19 +12,10 @@ export class VideoGameGrid extends React.Component {
 
     render() {
         return (
-            <Container>
-                <Row>
-                    <Col xs={6} md={4}>
-                        <LikeButton></LikeButton>
-                    </Col>
-                    <Col xs={6} md={4}>
-                        <LikeButton></LikeButton>
-                    </Col>
-                    <Col xs={6} md={4}>
-                        <LikeButton></LikeButton>
-                    </Col>
-                </Row>
-            </Container>
+            <div xs={6} md={4}>
+                <GameImg />
+                <LikeButton />
+            </div>
         )
     }
 }
