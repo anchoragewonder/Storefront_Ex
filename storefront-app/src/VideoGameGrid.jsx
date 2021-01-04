07b251eb3e6c23e82e063110266ff1f9b822fc4b
anchoragewonder <anchoragewonder@gmail.com>
2021-01-04 16:47:28 -0500

    update game grid render and json naming convention

--- storefront-app/src/VideoGameGrid.jsx
+++ storefront-app/src/VideoGameGrid.jsx
@@ -1,15 +1,5 @@
 import React from "react";
-import Col from 'react-bootstrap/Col'
 import { LikeButton } from './like_button';
-
-
-const gameList = ({ name, link }) => (
-    <div xs={6} md={4}>
-        <h4>{name}</h4>
-        <img src={link} />
-        <LikeButton />
-    </div>
-);
 
 export class VideoGameGrid extends React.Component {
     constructor(props) {
@@ -17,30 +7,30 @@ export class VideoGameGrid extends React.Component {
         this.state = {
             games: [],
         };
-
         this.getGames = this.getGames.bind(this);
     }
 
     async getGames() {
         const res = await fetch(`https://raw.githubusercontent.com/anchoragewonder/Storefront_Ex/JSX_Integration/storefront-app/src/Gamelist.json`);
         const data = await res.json();
-        console.log(data)
+        console.log(data);
         return data.results;
     }
 
     async componentDidMount() {
         const gameData = await this.getGames();
-        this.setState({ games });
+        this.setState({ games: gameData });
     }
 
     render() {
         return (
             <div>
-                {this.state.games.map(() => (
-                    <gameList
-                        name={`${games.game}`}
-                        link={`${games.Image}`}
-                    />
+                {this.state.games.map((gaming) => (
+                    <div xs={6} md={4}>
+                        <h4>{gaming.year.game}</h4>
+                        <img src={gaming.year.Image} />
+                        <LikeButton />
+                    </div>
                 ))}
             </div>
         );
